1fe2f2a6a2d41ac59149cab26ce1621ed0e4a9f8
szcleiton <souzacleiton54@gmail.com>
2025-09-13 15:48:15 -0300

    chore: configuring Drizzle ORM

---
+++ src/db/schema/users.ts
@@ -0,0 +1,14 @@
+import { pgTable, text, pgEnum, timestamp } from "drizzle-orm/pg-core";
+import { createId } from '@paralleldrive/cuid2'
+
+export const userRoleEnum = pgEnum("user_role", ["manager", "customer"]);
+
+export const users = pgTable("users", {
+  id: text("id").$defaultFn(() => createId()).primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  phone: text("phone"),
+  role: userRoleEnum("role").default("customer").notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
